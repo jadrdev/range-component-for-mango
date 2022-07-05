6f78b843bd9bb49beb6d2fd86beb15acbaf25537
Joshua Aarón Díaz Robayna <jadiaz@hiberus.com>
2022-07-05 11:43:04 +0200

    Added design and development dependencies

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import About from "./About";
+
+
+const Home = () => {
+  return (
+    <div>
+      <h1> Prueba </h1>
+      <About />
+    </div>
+  );
+};
+
+export default Home;
